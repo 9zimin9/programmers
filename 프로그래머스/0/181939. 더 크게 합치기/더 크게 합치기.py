7fad47639dc74104a43fa48b1a3ba8269268f1ec
namjimin_43 <mimi3123832@gmail.com>
2025-01-03 17:21:30 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    a=str(a)
+    b=str(b)
+    
+    ab= a+b
+    ba= b+a
+    
+    if int(ab) >= int(ba):
+        return int(ab)
+    else:
+        return int(ba)
+        
+    
